868e7f669bddebee3dd9a031ba1c1e57e9af3a0a
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2022-01-18 20:51:01 +0530

    initial header

---
+++ components/navbar.js
@@ -0,0 +1,43 @@
+function navbar()
+{
+    return `<div id="nav_bar_top">
+    <div id="nav_bar_top_left">
+    <li><a href="#">Offers</a></li>
+    <li><a href="#">Fanbook</a></li>
+    <li><a href="#"><span><i class="fas fa-mobile-alt"></i></span> Download App</a></li>
+    <li><a href="#">Tribe Membership</a></li>
+    
+   </div>
+
+
+   <div id="nav_bar_top_left">
+    <li><a href="#">Cantact Us</a></li>
+    <li><a href="#">Track Order</a></li>
+
+    </div>
+</div>
+<div id="nav_bar_search">
+    <div id="logo_menu">
+      
+        <li><a href="#"><img src="https://images.bewakoof.com/web/bewakoof-primary-logo-white-bg-2x-1635745564.png" alt="logo"></a></li>
+        <li><a href="#">MEN</a></li>
+        <li><a href="#">WOMEN</a></li>
+        <li><a href="#">MOBILE COVER</a></li>
+    </div>
+    <div id="nav_search_bar">
+         <li><input type="text" id="search_product_by_name" placeholder="search by product,category or collection"></li>
+        <li><a href="#">Login</a></li>
+        <li></li>
+        <li><span><i class="fas fa-lock"></i></span></li>
+    </div>
+</div>
+<hr>
+<div id="nav_bar_manu">
+    <li><a href="#"><P>MEN</P></a></li>
+    <li><a href="#"><P>WOMEN</P></a></li>
+    <li><a href="#"><P>ACCESSORIES</P></a></li>
+</div>
+`
+}
+
+export default navbar
