33278ca0b5328f7cc41e9d61f2249e03304a7265
Kalyani Sahu <sahu.kalyani879@gmail.com>
2022-01-18 21:12:26 +0530

    landing page partially completed

--- components/navbar.js
+++ components/navbar.js
@@ -20,16 +20,178 @@ function navbar()
     <div id="logo_menu">
       
         <li><a href="#"><img src="https://images.bewakoof.com/web/bewakoof-primary-logo-white-bg-2x-1635745564.png" alt="logo"></a></li>
-        <li><a href="#">MEN</a></li>
-        <li><a href="#">WOMEN</a></li>
-        <li><a href="#">MOBILE COVER</a></li>
+        <li><a href="#">MEN</a>
+         <div id="logo_sub_menu1">
+             <div id="sub_menu_item"><ul>
+                 <li><a href="#">TopWear</a></li>
+                 <li><a href="#">Half Sleeve T-Shirt</a></li>
+                 <li><a href="#">Full Slave T_Shirt</a></li>
+                 <li><a href="#">Printed T-Shirt</a></li>
+                 <li><a href="#"> Plain T-Shirt</a></li>
+                 <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                 <li><a href="#">Polo T-Shirt</a></li>
+                 <li><a href="#">Shirts</a></li>
+                 <li><a href="#">Kurtas</a></li>
+
+                </ul>
+                <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+            
+            </div>
+              
+         </div>
+        </li>
+        <li><a href="#">WOMEN</a>
+            <div id="logo_sub_menu2">
+                <div id="sub_menu_item"><ul>
+                    <li><a href="#">x</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+               
+               </div>
+              
+            </div></li>
+        <li><a href="#">MOBILE COVER</a>
+            <div id="logo_sub_menu3">
+                <div id="sub_menu_item"><ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+               
+               </div>
+              
+            </div></li>
+        
     </div>
     <div id="nav_search_bar">
          <li><input type="text" id="search_product_by_name" placeholder="search by product,category or collection"></li>
         <li><a href="#">Login</a></li>
-        <li></li>
-        <li><span><i class="fas fa-lock"></i></span></li>
+        <li><a href="#"><span><i class="far fa-heart"></i></span></a></li>
+        <li><a href="#"><span><i class="fas fa-lock"></i></span></a></li>
     </div>
+    
 </div>
 <hr>
 <div id="nav_bar_manu">
@@ -40,4 +202,4 @@ function navbar()
 `
 }
 
-export default navbar
+export default navbar ;
