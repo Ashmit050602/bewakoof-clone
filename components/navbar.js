f1bdbca8a72a718d66156961c4637713e648db9e
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2022-01-19 21:21:18 +0530

    product page added

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 function navbar()
 {
-    return `<div id="nav_bar_top">
+    return `   <div id="nav_bar_top">
     <div id="nav_bar_top_left">
     <li><a href="#">Offers</a></li>
     <li><a href="#">Fanbook</a></li>
@@ -20,16 +20,180 @@ function navbar()
     <div id="logo_menu">
       
         <li><a href="#"><img src="https://images.bewakoof.com/web/bewakoof-primary-logo-white-bg-2x-1635745564.png" alt="logo"></a></li>
-        <li><a href="#">MEN</a></li>
-        <li><a href="#">WOMEN</a></li>
-        <li><a href="#">MOBILE COVER</a></li>
+        <li><a href="#">MEN</a>
+         <div id="logo_sub_menu1">
+             <div id="sub_menu_item"><ul>
+                 <li><a href="#">TopWear</a></li>
+                 <li><a href="#">Half Sleeve T-Shirt</a></li>
+                 <li><a href="#">Full Slave T_Shirt</a></li>
+                 <li><a href="#">Printed T-Shirt</a></li>
+                 <li><a href="#"> Plain T-Shirt</a></li>
+                 <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                 <li><a href="#">Polo T-Shirt</a></li>
+                 <li><a href="#">Shirts</a></li>
+                 <li><a href="#">Kurtas</a></li>
+
+                </ul>
+                <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <hr>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#"><img src="https://images.bewakoof.com/t320/sacrifice-2-0-round-neck-vest-364859-1638211709-1.jpg" alt=""> ftrdcvhtrfdv</a></li>
+                    <li><a href="#"><img src="https://images.bewakoof.com/t320/sacrifice-2-0-round-neck-vest-364859-1638211709-1.jpg" alt=""></a></li>
+                    <li><a href="#"><img src="https://images.bewakoof.com/t320/sacrifice-2-0-round-neck-vest-364859-1638211709-1.jpg" alt=""></a></li>
+                    <li><a href="#"><img src="https://images.bewakoof.com/t320/sacrifice-2-0-round-neck-vest-364859-1638211709-1.jpg" alt=""></a></li>
+                    <li><a href="#"><img src="https://images.bewakoof.com/t320/sacrifice-2-0-round-neck-vest-364859-1638211709-1.jpg" alt=""></a></li>
+                    <!-- <li><a href="#"><img src="" alt=""></a></li>
+                    <li><a href="#"><img src="" alt=""></a></li>
+                    <li><a href="#"> <img src="" alt=""></a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li> -->
+
+                   </ul>
+            
+            </div>
+              
+         </div>
+        </li>
+        <li><a href="#">WOMEN</a>
+            <div id="logo_sub_menu2">
+                <div id="sub_menu_item"><ul>
+                    <li><a href="#">x</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+                   <ul>
+                    <li><a href="#">TopWear</a></li>
+                    <li><a href="#">Half Sleeve T-Shirt</a></li>
+                    <li><a href="#">Full Slave T_Shirt</a></li>
+                    <li><a href="#">Printed T-Shirt</a></li>
+                    <li><a href="#"> Plain T-Shirt</a></li>
+                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Polo T-Shirt</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Kurtas</a></li>
+
+                   </ul>
+               
+               </div>
+              
+            </div></li>
+        <li><a href="#">MOBILE COVER</a>
+            <div id="logo_sub_menu3">
+                <div id="sub_menu_item"><ul>
+                    <li><a href="#"><h2>POPULAR</h2></a></li>
+                    <li><a href="#"><h3>Xiaomi</h3></a></li>
+                    <li><a href="#">Realme 9 Power</a></li>
+                    <li><a href="#">Xiaomi Mi 10T</a></li>
+                    <li><a href="#">Xiaomi Redami K2</a></li>
+                    <li><a href="#"><h3>Sumsang Galaxy F62</h3></a></li><li><a href="#"></a></li>
+                    <li><a href="#">Sumsung Galaxy F41</a></li>
+                    <li><a href="#">Sumsumg Galaxy A30</a></li>
+                    
+
+                   </ul>
+                   <ul>
+                    <li><a href="#"><h3>Oneplus</h3></a></li>
+                    <li><a href="#">Oneplus 9R</a></li>
+                    <li><a href="#">Oneplus 8 pro</a></li>
+                    <li><a href="#">Oneplus 8T</a></li>
+                    <li><a href="#"> <H3>Realme</H3></a></li>
+                    <li><a href="#">Realme 6</a></li><li><a href="#"></a></li>
+                    <li><a href="#">Realme 5</a></li>
+                    
+
+                   </ul>
+                   <ul>
+                    <li><a href="#"><h3>Apple</h3></a></li>
+                    <li><a href="#">iphone 13</a></li>
+                    <li><a href="#">iphone 12</a></li>
+                    <li><a href="#">iphone 12 pro</a></li>
+                    <li><a href="#"><H3>Vivo</H3></a></li>
+                    <li><a href="#">vovo vii Pro</a></li><li><a href="#"></a></li>
+                    <li><a href="#"> Vivo X60</a></li>
+                    <li><a href="#">Vivo Y91</a></li>
+                    
+
+                   </ul>
+                   <hr>
+                   <ul>
+
+                   </ul>
+                   
+                   <ul>
+                    <li><a href="#"><span></span><img src="https://images.bewakoof.com/nav_menu/bewakoof-online-fashion-COTM-mobile-cover-navigation-box-desktop-1612275399.jpg" alt=""></a></li>
+                    
+
+                   </ul>
+               
+               </div>
+              
+            </div></li>
+        
     </div>
     <div id="nav_search_bar">
          <li><input type="text" id="search_product_by_name" placeholder="search by product,category or collection"></li>
         <li><a href="#">Login</a></li>
-        <li></li>
-        <li><span><i class="fas fa-lock"></i></span></li>
+        <li><a href="#"><span><i class="far fa-heart"></i></span></a></li>
+        <li><a href="#"><span><i class="fas fa-lock"></i></span></a></li>
     </div>
+    
 </div>
 <hr>
 <div id="nav_bar_manu">
